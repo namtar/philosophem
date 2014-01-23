2404080a3389b5a9d58c87b42c0680c6a20718dc
namtar <matthias.drummer@gmail.com>
2014-01-23 23:05:26 +0100

    some changes

--- main.c
+++ main.c
@@ -7,10 +7,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 short forks[3]; // 0 means false, 1 means true. This array represents the forks
-int waitingLeft[];
-int waitingRight[];
+int waitingLeft[3];
+int waitingRight[3];
 
 char deadlockMessage[] = {"The death of a clown."};
 char starvationMessage[] = {"Starvation."};
@@ -20,14 +21,29 @@ void doDeadlock();
 void doStarvation();
 void doBoring();
 
+void *philosoph(void *id);
+
 /*
  * The main function of the project.
  * 
  * Program with 4 threads, where 1 thread is for the observer and 3 threads for the philisophs themselves.
  * The fork array has to be checked with mutex variables.
+ * Damit das funktioniert muss dem compiler die Anweisung -pthread übergeben werden.
  */
 int main(int argc, char** argv) {
 
+    pthread_t philosoph1;
+    pthread_t philosoph2;
+    pthread_t philosoph3;
+    
+    pthread_create(&philosoph1, NULL, philosoph, (void *)0); // wir haben hier den pointer auf eine Funktion
+    pthread_create(&philosoph2, NULL, philosoph, (void *)1); // wir haben hier den pointer auf eine Funktion
+    pthread_create(&philosoph3, NULL, philosoph, (void *)2); // wir haben hier den pointer auf eine Funktion
+    
+    void *res; // void pointer für das result
+    pthread_join($philosoph1, &res); // wartet auf das Ende eines Threads.
+    
+    pthread_exit(NULL);
     return (EXIT_SUCCESS);
 }
 
@@ -41,4 +57,9 @@ void doStarvation() {
 
 void doBoring() {
     
+}
+
+void *philosoph(void *id) {
+    // do something.
+    pthread_exit(NULL);
 }
